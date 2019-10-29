0de6a32baf6a22e227b7ce94ebadccce397eb9e2
albert <trott.alik@gmail.com>
2019-10-29 17:49:56 +0500

    remove editorconfig

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,10 @@ import Player from 'audio-player-react'
 
 const App = () => {
   return (
-    <Player title='Some audio track' url='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'/>
+    <Player
+      title='Some audio track'
+      url='https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+    />
   )
 }
 export default App
